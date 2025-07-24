4bafb4ea4f628cdb4458e5549d92355ce876a2de
iago <iagopsilva073@gmail.com>
2025-07-23 22:48:28 -0300

    inicia o projeto

---
+++ src/test/java/dev/iago/gestao_funcionarios/GestaoFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.iago.gestao_funcionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
