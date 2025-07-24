baf404d00d3feb874af18ec49d8743373bcd66c1
iago <iagopsilva073@gmail.com>
2025-07-23 23:34:27 -0300

    testa infraestrutura

--- src/main/java/dev/iago/gestao_funcionarios/GestaoFuncionariosApplication.java
+++ src/main/java/dev/iago/gestao_funcionarios/GestaoFuncionariosApplication.java
@@ -2,9 +2,19 @@ package dev.iago.gestao_funcionarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class GestaoFuncionariosApplication {
+
+	@GetMapping
+	public String getHomeTeste() {
+		return "Hello world!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoFuncionariosApplication.class, args);
