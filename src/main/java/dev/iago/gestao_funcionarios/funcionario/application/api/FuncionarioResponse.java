8c2cbedcfcbffccd093e3b1a06441c38758bdd75
iago <iagopsilva073@gmail.com>
2025-07-27 14:02:52 -0300

    implementa request e reponse

--- src/main/java/dev/iago/gestao_funcionarios/funcionario/application/api/FuncionarioResponse.java
+++ src/main/java/dev/iago/gestao_funcionarios/funcionario/application/api/FuncionarioResponse.java
@@ -1,4 +1,10 @@
 package dev.iago.gestao_funcionarios.funcionario.application.api;
 
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
 public class FuncionarioResponse {
+    private UUID idFuncionario;
 }
