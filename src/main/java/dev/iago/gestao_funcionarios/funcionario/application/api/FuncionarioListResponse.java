e140f0c56ca3687d12f0f62e889bce20abbc2da7
iago <iagopsilva073@gmail.com>
2025-07-30 21:21:47 -0300

    implementa getTodos

--- src/main/java/dev/iago/gestao_funcionarios/funcionario/application/api/FuncionarioListResponse.java
+++ src/main/java/dev/iago/gestao_funcionarios/funcionario/application/api/FuncionarioListResponse.java
@@ -6,16 +6,27 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class FuncionarioListResponse {
     private UUID idFuncionario;
     private String nome;
     private String telefone;
 
     public static List<FuncionarioListResponse> converte(List<Funcionario> funcionarios) {
-        return null;
+        return funcionarios.stream()
+                .map(FuncionarioListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public FuncionarioListResponse(Funcionario funcionario) {
+        this.idFuncionario = funcionario.getIdFuncionario();
+        this.nome = funcionario.getNome();
+        this.telefone = funcionario.getTelefone();
     }
 }
